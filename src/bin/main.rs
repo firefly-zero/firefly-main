86c202d5460d15e88c8053995659ee61acea6776
gram <git@orsinium.dev>
2024-12-04 19:22:52 +0100

    draft display

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,8 +3,15 @@
 
 extern crate alloc;
 
+use display_interface_spi::SPIInterface;
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, prelude::*};
+use esp_hal::{
+    delay::Delay,
+    dma::{Dma, DmaPriority},
+    gpio::{Level, Output},
+    prelude::*,
+    spi::{master::Spi, SpiMode},
+};
 use esp_println::println;
 use firefly_hal::DeviceImpl;
 use firefly_runtime::{FullID, NetHandler, Runtime, RuntimeConfig};
@@ -32,10 +39,51 @@ fn run() -> Result<(), Error> {
     let peripherals = esp_hal::init(config);
     // println!("initializing UART...");
     // let uart = Uart::new(peripherals.UART1, peripherals.GPIO1, peripherals.GPIO2)?;
+
+    let sclk = peripherals.GPIO7;
+    let mosi = peripherals.GPIO6;
+    let cs = peripherals.GPIO5;
+    let miso = peripherals.GPIO2;
+    let dc = Output::new(peripherals.GPIO4, Level::Low);
+    let mut gpio_backlight = Output::new(peripherals.GPIO45, Level::Low);
+    let rst = Output::new(peripherals.GPIO48, Level::Low);
+
+    let dma = Dma::new(peripherals.DMA);
+    let dma_channel = dma.channel0;
+
+    let spi = Spi::new_with_config(
+        peripherals.SPI2,
+        esp_hal::spi::master::Config {
+            frequency: 60u32.MHz(),
+            ..esp_hal::spi::master::Config::default()
+        },
+    )
+    .with_sck(sclk)
+    .with_mosi(mosi)
+    .with_miso(miso)
+    .with_cs(cs)
+    .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
+    let mut delay = Delay::new();
+    gpio_backlight.set_high();
+
+    // let di = SPIInterface::new(spi, dc);
+    let di = new_no_cs(240 * 320 * 2, spi, dc);
+
+    // ESP32-S3-BOX display initialization workaround: Wait for the display to power up.
+    // If delay is 250ms, picture will be fuzzy.
+    // If there is no delay, display is blank
+    delay.delay_millis(500u32);
+
+    println!("initializing display...");
+    let display = mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, di)
+        .display_size(240, 320)
+        .orientation(mipidsi::options::Orientation::new())
+        .color_order(mipidsi::options::ColorOrder::Bgr)
+        .reset_pin(rst)
+        .init(&mut delay)?;
+
     println!("initializing device...");
     let device = DeviceImpl::new()?;
-    println!("initializing display...");
-    let display = Display::new();
     let config = RuntimeConfig {
         // id: None,
         id: Some(FullID::new(
--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
 pub enum Error {
     Uart(esp_hal::uart::Error),
     Runtime(firefly_runtime::Error),
+    Display,
+    Pin,
 }
 
 impl From<firefly_runtime::Error> for Error {
@@ -15,11 +17,22 @@ impl From<esp_hal::uart::Error> for Error {
     }
 }
 
+impl<T> From<mipidsi::error::InitError<T>> for Error {
+    fn from(v: mipidsi::error::InitError<T>) -> Self {
+        match v {
+            mipidsi::error::InitError::DisplayError => Self::Display,
+            mipidsi::error::InitError::Pin(_) => Self::Pin,
+        }
+    }
+}
+
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
-            Error::Uart(error) => write!(f, "UART error: {error:?}"),
-            Error::Runtime(error) => write!(f, "runtime error: {error}"),
+            Self::Uart(error) => write!(f, "UART error: {error:?}"),
+            Self::Runtime(error) => write!(f, "runtime error: {error}"),
+            Self::Display => write!(f, "display error"),
+            Self::Pin => write!(f, "pin error"),
         }
     }
 }
