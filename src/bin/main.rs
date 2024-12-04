d53d09b788258e4a864aefd35337449824c6a0f8
gram <git@orsinium.dev>
2024-12-04 20:39:53 +0100

    migrate to ili9341

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,10 @@
 
 extern crate alloc;
 
+use core::cell::RefCell;
+
 use display_interface_spi::SPIInterface;
+use embedded_hal_bus::spi::RefCellDevice;
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
@@ -16,6 +19,7 @@ use esp_println::println;
 use firefly_hal::DeviceImpl;
 use firefly_runtime::{FullID, NetHandler, Runtime, RuntimeConfig};
 use firefly_supervisor::*;
+use ili9341::{DisplaySize240x320, Ili9341, Orientation};
 
 #[entry]
 fn main() -> ! {
@@ -54,33 +58,40 @@ fn run() -> Result<(), Error> {
     let spi = Spi::new_with_config(
         peripherals.SPI2,
         esp_hal::spi::master::Config {
-            frequency: 60u32.MHz(),
+            frequency: 40u32.MHz(),
             ..esp_hal::spi::master::Config::default()
         },
     )
     .with_sck(sclk)
-    .with_mosi(mosi)
+    .with_mosi(mosi);
     // .with_miso(miso)
     // .with_cs(cs)
-    .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
+    // .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
     let mut delay = Delay::new();
     // gpio_backlight.set_high();
 
     // let di = SPIInterface::new(spi, dc);
-    let di = new_no_cs(240 * 320 * 2, spi, dc);
+    // let di = new_no_cs(240 * 320 * 2, spi, dc);
 
     // ESP32-S3-BOX display initialization workaround: Wait for the display to power up.
     // If delay is 250ms, picture will be fuzzy.
     // If there is no delay, display is blank
     delay.delay_millis(500u32);
 
+    let spi_bus = RefCell::new(spi);
+    let cs = Output::new(peripherals.GPIO5, Level::High);
+    let spi_device = RefCellDevice::new_no_delay(&spi_bus, cs).unwrap();
+    let spi_iface = SPIInterface::new(spi_device, dc);
+
     println!("initializing display...");
-    let display = mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, di)
-        .display_size(240, 320)
-        .orientation(mipidsi::options::Orientation::new())
-        .color_order(mipidsi::options::ColorOrder::Bgr)
-        .reset_pin(rst)
-        .init(&mut delay)?;
+    let display = Ili9341::new(
+        spi_iface,
+        rst,
+        &mut delay,
+        Orientation::Landscape,
+        DisplaySize240x320,
+    )
+    .unwrap();
 
     println!("initializing device...");
     let device = DeviceImpl::new()?;
@@ -88,7 +99,7 @@ fn run() -> Result<(), Error> {
         // id: None,
         id: Some(FullID::new(
             "demo".try_into().unwrap(),
-            "go-debug".try_into().unwrap(),
+            "go-triangle".try_into().unwrap(),
         )),
         device,
         display,
