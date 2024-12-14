8abce04caa19a421e7e46d67489a82ac1aa2f3a3
gram <git@orsinium.dev>
2024-12-14 17:14:03 +0100

    enable DMA, clean deps

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@
 extern crate alloc;
 
 use core::cell::RefCell;
-
-use display_interface_spi::SPIInterface;
 use embedded_hal_bus::spi::{ExclusiveDevice, RefCellDevice};
 use embedded_sdmmc::SdCard;
 use esp_backtrace as _;
@@ -14,13 +12,12 @@ use esp_hal::{
     dma::{Dma, DmaPriority},
     gpio::{Level, Output},
     prelude::*,
-    spi::{master::Spi, SpiMode},
+    spi::master::Spi,
 };
 use esp_println::println;
 use firefly_hal::DeviceImpl;
 use firefly_runtime::{FullID, NetHandler, Runtime, RuntimeConfig};
 use firefly_supervisor::*;
-use ili9341::{DisplaySize240x320, Ili9341, Orientation};
 
 #[entry]
 fn main() -> ! {
@@ -42,16 +39,15 @@ fn run() -> Result<(), Error> {
     config.cpu_clock = CpuClock::max();
     println!("initializing peripherals...");
     let peripherals = esp_hal::init(config);
-    esp_println::logger::init_logger(log::LevelFilter::Trace);
     // println!("initializing UART...");
     // let uart = Uart::new(peripherals.UART1, peripherals.GPIO1, peripherals.GPIO2)?;
 
     let sclk = peripherals.GPIO36;
     let mosi = peripherals.GPIO35;
-    // let cs = peripherals.GPIO5;
-    // let miso = peripherals.GPIO2;
+    let cs = peripherals.GPIO5;
+    let miso = peripherals.GPIO2;
     let dc = Output::new(peripherals.GPIO4, Level::Low);
-    // let mut gpio_backlight = Output::new(peripherals.GPIO45, Level::Low);
+    let mut gpio_backlight = Output::new(peripherals.GPIO45, Level::Low);
     let rst = Output::new(peripherals.GPIO48, Level::Low);
 
     let dma = Dma::new(peripherals.DMA);
@@ -65,24 +61,14 @@ fn run() -> Result<(), Error> {
         },
     )
     .with_sck(sclk)
-    .with_mosi(mosi);
-    // .with_miso(miso)
-    // .with_cs(cs)
-    // .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
+    .with_mosi(mosi)
+    .with_miso(miso)
+    .with_cs(cs)
+    .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
     let mut delay = Delay::new();
-    // gpio_backlight.set_high();
+    gpio_backlight.set_high();
 
-    // let di = SPIInterface::new(spi, dc);
-    // let di = new_no_cs(240 * 320 * 2, spi, dc);
-
-    // ESP32-S3-BOX display initialization workaround: Wait for the display to power up.
-    // If delay is 250ms, picture will be fuzzy.
-    // If there is no delay, display is blank
-    delay.delay_millis(500u32);
-
-    let cs = Output::new(peripherals.GPIO5, Level::High);
-    let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
-    let di = SPIInterface::new(spi_device, dc);
+    let di = new_no_cs(240 * 320 * 2, spi, dc);
 
     println!("initializing display...");
     let display = mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, di)
@@ -113,7 +99,6 @@ fn run() -> Result<(), Error> {
     .with_miso(miso)
     .with_mosi(mosi);
     let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
-    // let spi = SPIInterface::new(spi_device, dc);
     let sdcard = SdCard::new(spi_device, delay);
     if let Some(card_type) = sdcard.get_card_type() {
         println!("SD | card type: {card_type:?}");
