49979d9266c5927ca0f3bed7ff79bcd298e2ba19
gram <git@orsinium.dev>
2026-07-06 21:46:48 +0200

    OTA

--- src/v2.rs
+++ src/v2.rs
@@ -106,19 +106,11 @@ pub fn run_v2(peripherals: Peripherals) -> Result<(), Error> {
     println!("reading OTA state...");
     let mut flash = FlashStorage::new(peripherals.FLASH);
     let serial_number = read_serial(&mut flash);
-    let mut pt_buf = [0u8; esp_bootloader_esp_idf::partitions::PARTITION_TABLE_MAX_LEN];
-    let mut ota = OtaUpdater::new(&mut flash, &mut pt_buf).unwrap();
-    let part = ota.ota_data().unwrap().current_app_partition().unwrap();
-    let main_partition = match part {
-        AppPartitionSubType::Factory => 0,
-        AppPartitionSubType::Ota0 => 1,
-        AppPartitionSubType::Ota1 => 2,
-        _ => unreachable!(),
-    };
+    let main_partition = get_partition(&mut flash);
 
     println!("initializing device...");
     let rng = Rng::new();
-    let mut device = DeviceImpl::new(sd_spi, io_uart, usb_serial, rng)?;
+    let mut device = DeviceImpl::new(sd_spi, io_uart, usb_serial, rng, flash)?;
     let (io_version, io_partition) = device.get_io_chip_info().unwrap_or_default();
     let mut config = RuntimeConfig {
         id: None,
@@ -151,6 +143,19 @@ pub fn run_v2(peripherals: Peripherals) -> Result<(), Error> {
             }
         }
     }
+}
+
+fn get_partition(flash: &mut FlashStorage<'_>) -> u8 {
+    let mut pt_buf = [0u8; esp_bootloader_esp_idf::partitions::PARTITION_TABLE_MAX_LEN];
+    let mut ota = OtaUpdater::new(flash, &mut pt_buf).unwrap();
+    let part = ota.ota_data().unwrap().current_app_partition().unwrap();
+    let main_partition = match part {
+        AppPartitionSubType::Factory => 0,
+        AppPartitionSubType::Ota0 => 1,
+        AppPartitionSubType::Ota1 => 2,
+        _ => unreachable!(),
+    };
+    main_partition
 }
 
 fn get_firmware_version() -> (u8, u8, u8) {
